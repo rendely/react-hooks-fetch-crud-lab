71cbc17e52015d6a1dd197727c516ade9ec4380b
matthew rendely <matthew.rendely@gmail.com>
2023-04-12 22:03:41 -0700

    answer change patch request working

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,14 +1,31 @@
-import React, {useContext} from "react";
-import {QuestionsContext} from "./QuestionsContext";
+import React, { useContext } from "react";
+import { QuestionsContext } from "./QuestionsContext";
+const questionsAPI = 'http://localhost:4000/questions';
+
 
 function QuestionItem({ question }) {
   const { id, prompt, answers, correctIndex } = question;
   const { questions, setQuestions } = useContext(QuestionsContext);
 
-  function handleDelete(e){
+  function handleDelete(e) {
     e.preventDefault();
     const id = question.id;
     setQuestions(questions.filter(q => q.id !== id));
+  }
+
+  function handleChangeAnswer(e) {
+    const value = e.target.value;
+    const id = question.id;
+    fetch(questionsAPI + '/' + id, {
+      method: "PATCH",
+      headers: { "Content-type": "json/application" },
+      body: {correctIndex: value}
+    })
+      .then(r => r.json())
+      .then(updatedQuestion => {
+        setQuestions(questions.map(q => q.id === id ? updatedQuestion : q))
+      });
+
   }
 
   const options = answers.map((answer, index) => (
@@ -23,7 +40,7 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChangeAnswer}>{options}</select>
       </label>
       <button onClick={handleDelete}>Delete Question</button>
     </li>
